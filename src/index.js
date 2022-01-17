62ad689d494ce7a561ddafcbf24a7bca99ccdf24
annie-2686 <97668438+annie-2686@users.noreply.github.com>
2022-01-17 17:24:08 +0000

    description

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
 let apiKey = "c0ac58b13badefb603b7db3b6c4f7f72";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
