4c0e65bbd022d2d3e7eb4fa58388f897cfd881e2
annie-2686 <97668438+annie-2686@users.noreply.github.com>
2022-01-23 12:43:21 +0000

    added form in html

--- src/index.js
+++ src/index.js
@@ -38,18 +38,21 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-  let sunriseElement = document.querySelector("#sunrise");
-
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  sunriseElement.innerHTML = formateDate(response.data.sys.sunrise * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "c0ac58b13badefb603b7db3b6c4f7f72";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=japan&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
